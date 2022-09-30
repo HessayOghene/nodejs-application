25dcf4e2827db3314fc1af0507c122263a3eb3cf
Eseoghene Winifred Okposio <110950209+HessayOghene@users.noreply.github.com>
2022-09-30 22:16:46 +0100

    add sonar IP

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: '172.31.39.152:9000/',
+  serverUrl: 'http://172.31.39.152:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
